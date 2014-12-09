290309dfb8cb6e1f246e5a80713db531ce94ddd0
abikhatv <abikhattar.vincent@gmail.com>
2014-12-09 09:04:15 +0100

    tbf

---
+++ basic_motion/build-sys-linux-x86_64/CMakeFiles/basic_motion.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/basic_motion.dir/main.cpp.o"
+  "sdk/bin/basic_motion.pdb"
+  "sdk/bin/basic_motion"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/basic_motion.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
